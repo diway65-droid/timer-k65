496612ce8601fe41d5694c032a8d87e38d55db6c
diway65-droid <diway65@gmail.com>
2026-09-17 23:26:20 +0200

    Add files via upload

---
+++ pwa/sw.js
@@ -0,0 +1,46 @@
+const CACHE = 'kb65-minuteur-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './logo.webp',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-maskable-192.png',
+  './icons/icon-maskable-512.png',
+  './icons/apple-touch-icon.png',
+  './icons/favicon-64.png'
+];
+
+// Installation : on met tout en cache immédiatement
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+// Activation : on nettoie les anciennes versions de cache
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Stratégie : cache d'abord, réseau en secours (l'app marche 100% hors-ligne une fois installée)
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((resp) => {
+          const copy = resp.clone();
+          caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          return resp;
+        })
+        .catch(() => cached);
+    })
+  );
+});
